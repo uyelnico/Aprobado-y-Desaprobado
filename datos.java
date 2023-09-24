ead7d35668c75dbe67e3714a729c6168fb0685f4
Nicolay Chiazzaro Tosquellas <145890121+uyelnico@users.noreply.github.com>
2023-09-24 17:08:31 -0300

    Create datos.java

---
+++ datos.java
@@ -0,0 +1,15 @@
+package info;
+
+public class datos {
+
+    public static funcion[] estudiantes = {
+        new funcion("Alumno 1", 12385678, false, 6),
+        new funcion("Alumno 2", 23556789, true, 8),
+        new funcion("rgr", 34585690, true, 10)
+    };
+}
+
+/**
+ * @author Nicolay Chiazzaro
+ *
+ */
